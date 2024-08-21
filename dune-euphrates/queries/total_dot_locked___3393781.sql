7387b5bd78ebe3734e433edc0b0ac808763a216e
Shunji Zhan <shunhahaha@live.com>
2024-08-21 16:32:34 +0800

    update queries

--- dune-euphrates/queries/total_dot_locked___3393781.sql
+++ dune-euphrates/queries/total_dot_locked___3393781.sql
@@ -50,16 +50,3 @@ SELECT
 FROM daily_staked_filled
 ORDER BY pool_id, day;
 
-      SUM((eds."stake_pool_5" - eds."unstake_pool_5")) OVER (ORDER BY eds."day_timestamp" ASC) / 1e10 AS "ldot_starlay",
-
-        SUM((eds."stake_pool_0")) OVER (ORDER BY eds."day_timestamp" ASC) / 1e10 AS "lcdot_ldot_volume",
-        SUM((eds."stake_pool_1")) OVER (ORDER BY eds."day_timestamp" ASC) / 1e10 AS "lcdot_tdot_volume",
-        SUM((eds."stake_pool_2")) OVER (ORDER BY eds."day_timestamp" ASC) / 1e10 AS "dot_ldot_volume",
-        SUM((eds."stake_pool_3")) OVER (ORDER BY eds."day_timestamp" ASC) / 1e10 AS "dot_tdot_volume",
-        SUM((eds."stake_pool_4")) OVER (ORDER BY eds."day_timestamp" ASC) / 1e10 AS "dot_idot_volume",
-        SUM((eds."stake_pool_5")) OVER (ORDER BY eds."day_timestamp" ASC) / 1e10 AS "ldot_ildot_volume",
-        SUM(eds."net_stake") OVER (ORDER BY eds."day_timestamp" ASC) / 1e10 AS "total"
-    FROM dune.euphrates.result_euphrates_daily_stake AS eds
-    ORDER BY eds."day_timestamp" ASC
-) as dot_tvl
-LIMIT 1000
