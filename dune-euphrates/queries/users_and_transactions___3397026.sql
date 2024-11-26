beaab37064ca5279e9a2486e6895add4b67372b2
Shunji Zhan <shunhahaha@live.com>
2024-11-26 16:38:31 +0800

    added claim queries

--- dune-euphrates/queries/users_and_transactions___3397026.sql
+++ dune-euphrates/queries/users_and_transactions___3397026.sql
@@ -3,30 +3,44 @@
 -- query link: https://dune.com/queries/3397026
 
 
-WITH daily_stats AS (
+WITH weekly_stats AS (
     SELECT
-        DATE_TRUNC('day', "timestamp") as day,
-        COUNT(*) AS daily_tx_count,
-        COUNT(DISTINCT "recipient") AS daily_users
-    FROM query_3988562  /* euphrates tx v2 */
+        DATE_TRUNC('week', "timestamp") as day,
+        COUNT(*) AS weekly_tx_count,
+        COUNT(DISTINCT "recipient") AS weekly_users
+    FROM (
+        SELECT "timestamp", "recipient" FROM query_3988562  /* euphrates tx v3 */
+        UNION ALL 
+        SELECT "timestamp", "recipient" FROM query_4333079  /* euphrates claims v3 */
+    )  
     GROUP BY 1
 ),
 
 cumulative_stats AS (
     SELECT
         day,
-        SUM(daily_tx_count) OVER (ORDER BY day) AS cumulative_tx_count,
-        SUM(daily_users) OVER (ORDER BY day) AS cumulative_users
-    FROM daily_stats
+        SUM(weekly_tx_count) OVER (ORDER BY day) AS cumulative_tx_count,
+        SUM(weekly_users) OVER (ORDER BY day) AS cumulative_users,
+        (
+            SELECT COUNT(DISTINCT recipient)
+            FROM (
+                SELECT "recipient", "timestamp" FROM query_3988562  /* euphrates tx v3 */
+                UNION ALL 
+                SELECT "recipient", "timestamp" FROM query_4333079  /* euphrates claims v3 */
+            ) all_tx
+            WHERE "timestamp" <= day
+        ) AS cumulative_distinct_users
+    FROM weekly_stats
 )
+
 SELECT
     ds.day,
-    ds.daily_tx_count,
+    ds.weekly_tx_count,
     cs.cumulative_tx_count,
-    ds.daily_users,
+    ds.weekly_users,
     cs.cumulative_users
 FROM
-    daily_stats ds
+    weekly_stats ds
 JOIN
     cumulative_stats cs ON ds.day = cs.day
 ORDER BY
