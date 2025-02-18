c1b1b20e124b279a16d9245c664ccad97b2168ed
Shunji Zhan <shunhahaha@live.com>
2025-02-18 23:49:54 +0800

    dex apr

--- dune-acala-dex/queries/dex_swaps___3751506.sql
+++ dune-acala-dex/queries/dex_swaps___3751506.sql
@@ -3,25 +3,59 @@
 -- query link: https://dune.com/queries/3751506
 
 
-WITH dex_swap_raw AS (
-    SELECT 
-        block_time,
-        block_number,
-        block_hash,
-        extrinsic_id,
-        extrinsic_hash,
-        event_id,
+/* ---------------
+   similar to dex_swap, but for a single A => B => C swap
+   this will return 2 swaps: A => B and B => C  
+                                          --------------- */
+with dex_swaps_with_idx AS (
+    SELECT
+        *,
         JSON_EXTRACT_SCALAR(data, '$[0]') AS address,
-        JSON_EXTRACT(data, '$[1][0]') AS token_in_json,
-        JSON_ARRAY_GET(JSON_EXTRACT(data, '$[1]'), -1) AS token_out_json,
-        JSON_VALUE(data, 'strict $[2][0]') AS amount_in_varchar,
-        JSON_VALUE(data, 'strict $[2][last]') AS amount_out_varchar
+        CONCAT('$[2][', CAST(idx AS VARCHAR), ']') as path_in,
+        CONCAT('$[2][', CAST(idx + 1 AS VARCHAR), ']') as path_out
     FROM acala.events
+    CROSS JOIN (
+        SELECT 0 AS idx
+        UNION ALL
+        SELECT 1
+        UNION ALL
+        SELECT 2
+        UNION ALL
+        SELECT 3
+     ) as indices
     WHERE section = 'dex'
     AND   method = 'Swap'
 ),
 
-dex_swap_raw_extracted AS (
+dex_swaps_splited AS (
+    SELECT
+        *,
+        JSON_EXTRACT(data, CONCAT('$[1][', CAST(idx AS VARCHAR), ']')) AS token_in_json,
+        JSON_EXTRACT(data, CONCAT('$[1][', CAST(idx + 1 AS VARCHAR), ']')) AS token_out_json,
+        COALESCE(
+          TRY(JSON_EXTRACT_SCALAR(data, path_in)),
+          CAST(
+            varbinary_to_uint256 (
+              from_hex(
+                JSON_EXTRACT_SCALAR(data, path_in)
+              )
+            ) AS varchar
+          )
+        ) AS amount_in_varchar,
+        COALESCE(
+          TRY(JSON_EXTRACT_SCALAR(data, path_out)),
+          CAST(
+            varbinary_to_uint256 (
+              from_hex(
+                JSON_EXTRACT_SCALAR(data, path_out)
+              )
+            ) AS varchar
+          )
+        ) AS amount_out_varchar
+    FROM dex_swaps_with_idx
+),
+
+dex_swaps_extracted AS (
     SELECT
         *,
         CASE
@@ -46,13 +80,14 @@ dex_swap_raw_extracted AS (
               '"}'
             )
           )
-        WHEN JSON_EXTRACT_SCALAR(X.token_in_json, '$.erc20') IS NOT NULL THEN (
+          WHEN JSON_EXTRACT_SCALAR(X.token_in_json, '$.erc20') IS NOT NULL THEN (
             CONCAT(
               '{"Erc20":"',
               JSON_EXTRACT_SCALAR(X.token_in_json, '$.erc20'),
               '"}'
             )
           )
+          ELSE '???'
         END AS token_in_varchar,
 
         CASE
@@ -84,8 +119,10 @@ dex_swap_raw_extracted AS (
               '"}'
             )
           )
+          ELSE '???'
         END AS token_out_varchar
-    FROM dex_swap_raw X
+    FROM dex_swaps_splited X
+    WHERE token_out_json IS NOT NULL
 ),
 
 dex_swap_parsed AS (
@@ -105,15 +142,16 @@ dex_swap_parsed AS (
         B.decimals AS decimals_in,
         C.symbol AS token_out,
         C.decimals AS decimals_out
-    FROM dex_swap_raw_extracted A
+    FROM dex_swaps_extracted A
     LEFT JOIN query_4397191 B  -- acala assets
     ON A.token_in_varchar = B.asset
     LEFT JOIN query_4397191 C  -- acala assets
     ON A.token_out_varchar = C.asset
 ),
 
-dex_swap_formatted AS (
-  SELECT
+
+dex_swap_with_pool_name AS (
+    SELECT
       D.block_time,
       D.address,
       D.amount_in_uint256 / POWER(10, D.decimals_in) AS amount_in,
@@ -122,35 +160,46 @@ dex_swap_formatted AS (
       D.token_out,
       D.block_number,
       D.extrinsic_hash as tx_hash,
-      DATE_TRUNC('day', D.block_time) AS day
-  FROM dex_swap_parsed D
-)
+      COALESCE(
+          p1.pool_name,  -- matches token_in/token_out
+          p2.pool_name,  -- matches token_out/token_in
+          '???'
+      ) as pool_name
+    FROM dex_swap_parsed D
+    LEFT JOIN query_4740395 p1   -- dex_pool_names
+        ON CONCAT(D.token_in, '/', D.token_out) = p1.pool_name
+    LEFT JOIN query_4740395 p2   -- dex_pool_names
+        ON CONCAT(D.token_out, '/', D.token_in) = p2.pool_name
+    ORDER BY 1 DESC
+),
 
-
+dex_swap_usd AS (
 SELECT
-    E.block_time,
-    E.address,
-    E.amount_in,
-    E.token_in,
-    E.amount_out,
-    E.token_out,
+    A.block_time,
+    A.address,
+    A.amount_in,
+    A.token_in,
+    A.amount_out,
+    A.token_out,
     CASE
-        WHEN E.token_in IN ('DOT', 'lcDOT') THEN E.amount_in * dot_price.price
-        WHEN E.token_in IN ('JITOSOL') THEN E.amount_in * jitosol_price.price
-        WHEN E.token_in IN ('AUSD', 'USDC') THEN E.amount_in
-        WHEN E.token_out IN ('DOT', 'lcDOT') THEN E.amount_out * dot_price.price
-        WHEN E.token_out IN ('JITOSOL') THEN E.amount_out * jitosol_price.price
-        WHEN E.token_out IN ('AUSD', 'USDC') THEN E.amount_out
+        WHEN A.token_in IN ('DOT', 'lcDOT') THEN A.amount_in * dot_price.price
+        WHEN A.token_in IN ('JITOSOL') THEN A.amount_in * jitosol_price.price
+        WHEN A.token_in IN ('AUSD', 'USDC', 'USDT') THEN A.amount_in
+        WHEN A.token_out IN ('DOT', 'lcDOT') THEN A.amount_out * dot_price.price
+        WHEN A.token_out IN ('JITOSOL') THEN A.amount_out * jitosol_price.price
+        WHEN A.token_out IN ('AUSD', 'USDC', 'USDT') THEN A.amount_out
         ELSE 0
     END AS usd_value,
-    E.block_number,
-    E.tx_hash
-FROM dex_swap_formatted E
-LEFT JOIN query_3989007 as dot_price
-    ON E.day = dot_price.day 
-    AND dot_price.symbol = 'DOT'
-LEFT JOIN query_3989007 as jitosol_price
-    ON E.day = jitosol_price.day 
-    AND jitosol_price.symbol = 'JITOSOL'
-WHERE E.day != DATE '2022-08-14'
-ORDER BY 1 DESC
+    A.block_number,
+    A.tx_hash
+    FROM dex_swap_formatted A
+    LEFT JOIN query_3989007 AS dot_price
+        ON A.day = dot_price.day 
+        AND dot_price.symbol = 'DOT'
+    LEFT JOIN query_3989007 AS jitosol_price
+        ON A.day = jitosol_price.day 
+        AND jitosol_price.symbol = 'JITOSOL'
+    WHERE A.day != DATE '2022-08-14'
+      AND A.pool_name != '???'
+    ORDER BY 1 DESC
+)
--- dune-acala-dex/queries/pol_tvl_ausd_intr___3799562.sql
+++ dune-acala-dex/queries/pol_tvl_ausd_intr___3799562.sql
@@ -1,5 +1,5 @@
 -- part of a query repo
--- query name: pol_tvl_ausd_intr
+-- query name: pool_tvl_ausd_intr
 -- query link: https://dune.com/queries/3799562
 
 
@@ -7,6 +7,6 @@ SELECT
     date,
     token0_tvl AS aseed_tvl,
     token1_tvl AS intr_tvl
-FROM query_3782346 AS pool_tvl
+FROM dune.euphrates.result_dex_pool_tvl AS pool_tvl
 WHERE pool_name = 'AUSD/INTR'
 AND token0_tvl > 0
